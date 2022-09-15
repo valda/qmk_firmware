64665ba8cd8c2b685d2ec19944363bf21e8e8176
YAMAGUCHI Seiji <yamaguchi@hld.co.jp>
2022-09-15 17:35:19 +0900

    キーマップ更新

--- keyboards/helix/rev2/keymaps/valda/keymap.c
+++ keyboards/helix/rev2/keymaps/valda/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT, \
-      ADJUST,  KC_MEH,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  SFT_MHEN,SFT_HENK,KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      ADJUST,  KC_MEH,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_MHEN, KC_HENK, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
   /* Lower
