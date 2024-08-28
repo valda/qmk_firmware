b3bf040f4bd08e6d1286f03e339282639d12359d
YAMAGUCHI Seiji <yamaguchi@hld.co.jp>
2024-08-28 16:40:49 +0900

    ビルドオプション OLED_ENABLE = yes をコメントアウト

--- keyboards/helix/rev2/keymaps/valda/rules.mk
+++ keyboards/helix/rev2/keymaps/valda/rules.mk
@@ -5,7 +5,7 @@ LTO_ENABLE = yes  # if firmware size over limit, try this option
 # Helix Spacific Build Options
 # you can uncomment and edit follows 7 Variables
 #  jp: 以下の7つの変数を必要に応じて編集し、コメントアウトをはずします。
-OLED_ENABLE = yes            # OLED_ENABLE
+# OLED_ENABLE = no            # OLED_ENABLE
 # LOCAL_GLCDFONT = no         # use each keymaps "helixfont.h" insted of "common/glcdfont.c"
 # LED_BACK_ENABLE = no        # LED backlight (Enable WS2812 RGB underlight.)
 # LED_UNDERGLOW_ENABLE = no   # LED underglow (Enable WS2812 RGB underlight.)
