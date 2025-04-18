335a28d6494f4eb0196522e228422caefc780b18
YAMAGUCHI Seiji <valda@underscore.jp>
2025-04-18 15:28:04 +0900

    rebase origin/master

--- keyboards/helix/rev2/keymaps/valda/keymap.c
+++ keyboards/helix/rev2/keymaps/valda/keymap.c
@@ -152,11 +152,6 @@ float tone_plover_gb[][2]  = SONG(PLOVER_GOODBYE_SOUND);
 bool TOG_STATUS = false;
 int RGB_current_mode;
 
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
 // Setting ADJUST layer RGB back to default
 void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
   if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
@@ -176,7 +171,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         #ifdef AUDIO_ENABLE
           PLAY_SONG(tone_qwerty);
         #endif
-        persistent_default_layer_set(1UL<<_QWERTY);
+        set_single_persistent_default_layer(_QWERTY);
       }
       return false;
       break;
@@ -185,7 +180,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     /*     #ifdef AUDIO_ENABLE */
     /*       PLAY_SONG(tone_colemak); */
     /*     #endif */
-    /*     persistent_default_layer_set(1UL<<_COLEMAK); */
+    /*     set_single_persistent_default_layer(_COLEMAK); */
     /*   } */
     /*   return false; */
     /*   break; */
@@ -194,7 +189,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     /*     #ifdef AUDIO_ENABLE */
     /*       PLAY_SONG(tone_dvorak); */
     /*     #endif */
-    /*     persistent_default_layer_set(1UL<<_DVORAK); */
+    /*     set_single_persistent_default_layer(_DVORAK); */
     /*   } */
     /*   return false; */
     /*   break; */
@@ -263,30 +258,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       #endif
       return false;
       break;
-    /* case EISU: */
-    /*   if (record->event.pressed) { */
-    /*     if(keymap_config.swap_lalt_lgui==false){ */
-    /*       register_code(KC_LANG2); */
-    /*     }else{ */
-    /*       SEND_STRING(SS_LALT("`")); */
-    /*     } */
-    /*   } else { */
-    /*     unregister_code(KC_LANG2); */
-    /*   } */
-    /*   return false; */
-    /*   break; */
-    /* case KANA: */
-    /*   if (record->event.pressed) { */
-    /*     if(keymap_config.swap_lalt_lgui==false){ */
-    /*       register_code(KC_LANG1); */
-    /*     }else{ */
-    /*       SEND_STRING(SS_LALT("`")); */
-    /*     } */
-    /*   } else { */
-    /*     unregister_code(KC_LANG1); */
-    /*   } */
-    /*   return false; */
-    /*   break; */
+    case EISU:
+      if (record->event.pressed) {
+        if (is_mac_mode()) {
+          register_code(KC_LNG2);
+        } else {
+          SEND_STRING(SS_LALT("`"));
+        }
+      } else {
+        unregister_code(KC_LNG2);
+      }
+      return false;
+      break;
+    case KANA:
+      if (record->event.pressed) {
+        if (is_mac_mode()) {
+          register_code(KC_LNG1);
+        } else {
+          SEND_STRING(SS_LALT("`"));
+        }
+      } else {
+        unregister_code(KC_LNG1);
+      }
+      return false;
+      break;
     case RGBRST:
       #ifdef RGBLIGHT_ENABLE
         if (record->event.pressed) {
